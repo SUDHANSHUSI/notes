80b3b93956e0a59056708151a9e07b4ada039360
UserOne <sudhanshusingh0401@gmail.com>
2023-05-27 15:11:05 +0530

    added navigation and css to details pages

--- FoodCorner/src/app/app.module.ts
+++ FoodCorner/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { RatingModule } from 'ng-starrating';
+import { SearchComponent } from './components/pages/search/search.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { TagComponent } from './components/pages/tag/tag.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent,
+    FoodPageComponent,
+    TagComponent
   ],
   imports: [
     BrowserModule,
--- FoodCorner/src/app/services/food.service.ts
+++ FoodCorner/src/app/services/food.service.ts
@@ -1,14 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Food } from '../shared/model/Food';
-import { sample_foods } from 'src/data';
+import { sample_foods, sample_tags } from 'src/data';
+import { Tag } from '../shared/model/Tag';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoodService {
+  constructor() {}
+  getAll(): Food[] {
+    return sample_foods;
+  }
+  // search food
+  getAllFoodBySearchTerm(searchTerm: string) {
+    return this.getAll().filter((food) =>
+      food.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+  // get all tags
+  getAllTags(): Tag[] {
+    return sample_tags;
+  }
+  // get food by Tags
+  getAllFoodByTags(tag: string): Food[] {
+    return tag === 'All'
+      ? this.getAll()
+      : this.getAll().filter((food) => food.tags?.includes(tag));
+  }
 
-  constructor() { }
-  getAll():Food[]{
-    return sample_foods
+  // Get Food By Id
+  getFoodById(foodId: string) {
+    return this.getAll().find((food) => food.id) ?? new Food();
   }
 }
